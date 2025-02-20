e3fb06e2277060fb4bdcaff5752ad9c10be1d3a5
javier-aguilar <jvr.agx@gmail.com>
2025-02-19 21:48:43 -0700

    Add logic to connect to hue bridge

--- lib/hue_ruby.rb
+++ lib/hue_ruby.rb
@@ -1,5 +1,25 @@
+require "net/http"
+require "json"
+require "pry"
+
 class HueRuby
-  def self.hi
-    puts "Hello world!"
+  def self.connect
+    uri = URI.parse("#{bridge.ip}/api")
+
+    header = {'Content-Type': 'text/json'}
+    body = { devicetype: "ruby", generateclientkey: true }
+
+    # Create the HTTP objects
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = body.to_json
+
+    # Send the request
+    response = JSON(http.request(request).body)[0]
+    
+    if response["error"]
+      raise ArgumentError, response["error"]["description"] 
+    end
+
   end
 end
