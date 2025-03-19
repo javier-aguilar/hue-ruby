d756a930414eab78f19c8df2e03d5388bed051ec
javier-aguilar <jvr.agx@gmail.com>
2025-03-19 00:00:50 -0600

    Add get_bridge_ip_address and get_lights method

--- lib/hue_ruby.rb
+++ lib/hue_ruby.rb
@@ -3,8 +3,16 @@ require "json"
 require "pry"
 
 class HueRuby
+  def self.get_bridge_ip_address
+    uri = URI("https://discovery.meethue.com/")
+    response = Net::HTTP.get(uri)
+    
+    ip_address = JSON.parse(response)[0]["internalipaddress"]
+    "http://#{ip_address}"
+  end
+  
   def self.connect
-    uri = URI.parse("#{bridge.ip}/api")
+    uri = URI.parse("#{get_bridge_ip_address}/api")
 
     header = {'Content-Type': 'text/json'}
     body = { devicetype: "ruby", generateclientkey: true }
@@ -16,10 +24,32 @@ class HueRuby
 
     # Send the request
     response = JSON(http.request(request).body)[0]
-    
+
     if response["error"]
       raise ArgumentError, response["error"]["description"] 
     end
-
   end
+
+  def self.get_lights
+    uri = URI.parse("#{get_bridge_ip_address}/api/#{username}/lights")
+
+    header = {
+      'Content-Type': 'text/json',
+      'hue-application-key': app_key
+    }
+
+    # Create the HTTP objects
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri, header)
+
+    # Send the request
+    response = http.request(request)
+    # print JSON.parse(response.body)
+
+    if response["error"]
+      raise ArgumentError, response["error"]["description"] 
+    end
+  end
+
+
 end
